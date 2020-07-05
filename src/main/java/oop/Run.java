549906a2cc3c4e1f73dfbdce2e080ae1b9ace430
S-Zalevskyi <60276234+S-Zalevskyi@users.noreply.github.com>
2020-07-05 11:03:26 +0200

    Create registration form

--- src/main/java/oop/Run.java
+++ src/main/java/oop/Run.java
@@ -1,15 +1,39 @@
 package oop;
 
+import oop.controller.UserController;
 import oop.model.User;
 import oop.model.enums.Gender;
 
+import java.util.Scanner;
+
 public class Run {
     public static void main(String[] args) {
-        User u1 = new User("Adam", "Kowalski", "ak@ak.pl", "ak", "123123123", Gender.MAN);
-        User u2 = new User("Jan", "Nowak", "jn@jn.pl", "jn", "333123123", Gender.MAN);
-        User u3 = new User("Anna", "Lis", "al@al.pl", "al", "444123123", Gender.WOMAN);
-        System.out.println(u1);
-        System.out.println(u2);
-        System.out.println(u3);
+        UserController uc = new UserController();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Co chcesz zrobic? \n1) Rejestracja \n2) Logowanie \nQ. Wyjscie");
+            String choice = scanner.nextLine().toUpperCase();
+            if (choice.equals("1")) {
+                System.out.println("Podaj imie: ");
+                String name = scanner.nextLine();
+                System.out.println("Podaj nazwisko: ");
+                String lastName = scanner.nextLine();
+                System.out.println("Podaj email: ");
+                String mail = scanner.nextLine();
+                System.out.println("Podaj haslo: ");
+                String password = scanner.nextLine();
+                System.out.println("Podaj plec (M/K): ");
+                String genderInput = scanner.nextLine();
+                Gender gender = genderInput.equals("M") ? Gender.MAN : Gender.WOMAN;
+                System.out.println("Podaj telefon: ");
+                String phone = scanner.nextLine();
+                uc.registerUser(new User (name, lastName, mail, password, phone, gender));
+
+            }else if (choice.equals("2")){
+
+            }else if (choice.equals("Q")) {
+                System.out.println("Wyjscie");
+            } break;
+        }
     }
 }
--- src/main/java/oop/controller/UserController.java
+++ src/main/java/oop/controller/UserController.java
@@ -1,4 +1,51 @@
 package oop.controller;
 
-public class UserController {
+import oop.controller.enums.UserField;
+import oop.model.User;
+import oop.model.enums.Role;
+
+import java.util.List;
+import java.util.Set;
+
+public class UserController implements UserControllerTempl {
+    @Override
+    public void registerUser(User user) {
+        users.add(user);
+        System.out.println("Dodano nowego uzytkownika: " + user.getEmail());
+    }
+
+    @Override
+    public boolean login(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public User findUser(int userId) {
+        return null;
+    }
+
+    @Override
+    public void updateUserPassword(int userId, String newPassword) {
+
+    }
+
+    @Override
+    public void deleteUserById(int usrId) {
+
+    }
+
+    @Override
+    public void updateRole(int userId, Set<Role> roles) {
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsersOrderByArg(UserField userField, boolean asc) {
+        return null;
+    }
 }
